743ab6b0088da0b33f6097a4000fd92006d21cc5
B3ND3L <delahais.b@gmail.com>
2019-01-26 00:50:09 +0100

    Add event switch + add npm start command

--- src/server.js
+++ src/server.js
@@ -1,13 +1,42 @@
 WebSocket = require('ws');
+const port = 30682;
 
 //SERVER
-var wss = new WebSocket.Server({
-    port: 30682
+
+const wss = new WebSocket.Server({
+    port: port
 });
+console.log("Server listen on port "+port);
 
-
-wss.on('connection', function(client) {
+wss.on('connection', function(client, request) {
+    const ip = request.connection.remoteAddress;
+    console.log('Request received from ' + ip);
     client.on('message', function(message) {
-        console.log(message);
+        const data = JSON.parse(message);
+
+        switch (data.event) {
+            case "position" :
+                console.log('New player position !');
+                console.log("x : " + data.x + ", y : " + data.y);
+                break;
+            case "collectRsc" : //id
+                console.log("Collect ressource");
+                break;
+            case "activateFlux": //id
+                console.log("Activate Flux");
+                break;
+            case "dropRscHome" : //5
+                console.log("Drop ressource to home");
+                break;
+            case "getRsvHome" : //5
+                console.log("Get ressource from home");
+                break;
+            case "motivateFlux": //id
+                break;
+            default:
+                console.log("Unrecognize event ...");
+                console.log(message);
+                break;
+        }
     });
 });
