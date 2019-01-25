285c38b8b2ab8f0a875add01c4d1632937cec4af
B3ND3L <delahais.b@gmail.com>
2019-01-26 00:02:48 +0100

    First websocket server working

---
+++ src/clientTest.js
@@ -0,0 +1,8 @@
+WebSocket = require('ws');
+
+const ws = new WebSocket('ws://localhost:30682');
+
+
+ws.on('open', function open() {
+    console.log('connected to central');
+});
--- src/server.js
+++ src/server.js
@@ -1,19 +1,7 @@
 WebSocket = require('ws');
-/*
-
-Client
-
-const ws = new WebSocket('ws://localhost:30682');
-
-
-ws.on('open', function open() {
-    console.log('connected to central');
-    identify();
-});
-*/
-
 
 //SERVER
-var wss = new WebSocketServer({
-    server: server
+var wss = new WebSocket.Server({
+    port: 30682
 });
+
