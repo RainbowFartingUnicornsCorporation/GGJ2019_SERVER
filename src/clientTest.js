0f40221d606e7fb2492deb3be3028b4536d8aeef
B3ND3L <delahais.b@gmail.com>
2019-01-26 00:19:36 +0100

    Server show in log received messages

--- src/clientTest.js
+++ src/clientTest.js
@@ -5,4 +5,7 @@ const ws = new WebSocket('ws://localhost:30682');
 
 ws.on('open', function open() {
     console.log('connected to central');
+
+
+    ws.send('cctvvmb');
 });
--- src/server.js
+++ src/server.js
@@ -5,3 +5,9 @@ var wss = new WebSocket.Server({
     port: 30682
 });
 
+
+wss.on('connection', function(client) {
+    client.on('message', function(message) {
+        console.log(message);
+    });
+});
